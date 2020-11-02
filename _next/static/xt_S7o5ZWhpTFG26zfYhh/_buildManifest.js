a2eb447be3c2f76f66efee536cacf91c807e00f7
Tony Brobston <anthonybrobston@live.com>
2020-11-02 10:00:24 -0600

    Updates

---
+++ _next/static/xt_S7o5ZWhpTFG26zfYhh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-53e0565366233bab0fb5.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9865e0cbeec5244a1a14.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
